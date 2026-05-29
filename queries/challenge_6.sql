09fb0abd36327268591648401a51303f936f65df
kbanning04 <kbanning04@gmail.com>
2026-05-29 15:33:59 +0100

    Challenge 6

--- queries/challenge_6.sql
+++ queries/challenge_6.sql
@@ -6,3 +6,26 @@
 
 -- Results should be sorted alphabetically by supplier and category.
 
+SELECT
+    s.company_name AS supplier,
+    c.category_name AS category,
+    floor(AVG(p.unit_price)::integer) AS average_price
+FROM
+    suppliers AS s 
+JOIN 
+    products AS p 
+USING 
+    (supplier_id)
+JOIN 
+    categories AS c 
+USING
+    (category_id)
+WHERE
+    c.category_name ILIKE '%m%'
+GROUP BY 
+    s.company_name,
+    c.category_name
+ORDER BY 
+    s.company_name ASC,
+    c.category_name ASC
+
