68cdb49430f7d8bbfc2432dd623174d7de379f53
kbanning04 <kbanning04@gmail.com>
2026-05-29 16:13:32 +0100

    Challenge 9

--- queries/challenge_9.sql
+++ queries/challenge_9.sql
@@ -2,3 +2,12 @@
 
 -- Results should be ordered in descending order of employment year, with ties broken by sorting alphabetically by last name (ascending).
 
+
+SELECT
+    e.title_of_courtesy || ' ' || e.first_name || ' ' || e.last_name AS employee_full_name
+FROM 
+    employees AS e
+ORDER BY 
+    EXTRACT(YEAR FROM e.hire_date) DESC,
+    e.last_name ASC
+;
