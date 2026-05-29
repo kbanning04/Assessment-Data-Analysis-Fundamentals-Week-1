5b0dd0cd715820b67ca7b9541ebc8248c058a315
kbanning04 <kbanning04@gmail.com>
2026-05-29 14:51:16 +0100

    Attempt at challenge 2

--- queries/challenge_2.sql
+++ queries/challenge_2.sql
@@ -1,2 +1,9 @@
 -- A query that displays the total integer value of all products in stock as "stock_value".
 
+-- SUM all units_in_stock for products
+
+SELECT 
+    (SUM(units_in_stock)) AS stock_value
+FROM
+    products
+;
