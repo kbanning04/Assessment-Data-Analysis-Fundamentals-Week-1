d7a99f932c21dd56518babb13ff05f987710b9fc
kbanning04 <kbanning04@gmail.com>
2026-05-29 17:08:54 +0100

    Challenge 11 and attempt at Challenge 12'

--- queries/challenge_11.sql
+++ queries/challenge_11.sql
@@ -4,3 +4,16 @@
 
 -- The percentage should be rounded to 2 d.p.
 
+WITH discounted_orders AS(
+    SELECT 
+        (COUNT(DISTINCT order_id)*1.0) AS number_of_discounts
+    FROM 
+        order_details
+    WHERE 
+        discount != 0
+)
+SELECT
+    ROUND(((number_of_discounts / (SELECT COUNT(DISTINCT order_id) FROM order_details)) * 100),2) AS percentage_discounted
+FROM
+    discounted_orders
+;
--- queries/challenge_12.sql
+++ queries/challenge_12.sql
@@ -8,3 +8,42 @@
 
 -- All values should be rounded to 2 d.p. for display (but otherwise kept at full precision)
 
+
+WITH discount_price AS (
+    SELECT
+        o.order_id, 
+        (SUM((o.unit_price * o.quantity))) * o.discount AS new_price
+    FROM
+        order_details AS o
+    GROUP BY
+        o.order_id, o.discount
+),
+normal_price AS (
+    SELECT
+        o.order_id, 
+        (SUM((o.unit_price * o.quantity))) AS price
+    FROM
+        order_details AS o
+    GROUP BY
+        order_id
+)
+SELECT 
+    order_id,
+    n.price AS expected_price,
+    d.new_price AS actual_price,
+    (n.price - d.new_price) AS price_difference
+FROM 
+    normal_price AS n
+JOIN 
+    discount_price AS d
+USING
+    (order_id)
+WHERE
+    d.new_price != 0
+ORDER BY
+    price_difference DESC
+LIMIT 5
+;
+
+
+
