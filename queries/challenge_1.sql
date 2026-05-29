97b7861e5d3b05fd8d6c8109e606b10527975a04
kbanning04 <kbanning04@gmail.com>
2026-05-29 14:51:47 +0100

    Challenge 1 had modifications?

--- queries/challenge_1.sql
+++ queries/challenge_1.sql
@@ -1,6 +1,6 @@
 -- A query that displays the total count of customers under the column "total_customers".
 SELECT 
-    COUNT(customer_id)
+    COUNT(customer_id) AS total_customers
 FROM 
     customers
 ;
