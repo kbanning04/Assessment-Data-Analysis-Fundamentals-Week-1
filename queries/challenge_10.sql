dacc6626a3233ece6c0da1dfcb88f9b7b294b1f3
kbanning04 <kbanning04@gmail.com>
2026-05-29 16:35:18 +0100

    Challenge 10

--- queries/challenge_10.sql
+++ queries/challenge_10.sql
@@ -4,3 +4,37 @@
 
 -- Results should be sorted alphabetically by category.
 
+WITH rank_categories AS( 
+    SELECT 
+        c.category_name,
+        p.product_name,
+        RANK() 
+            OVER( 
+                PARTITION BY c.category_name 
+                ORDER BY p.unit_price DESC)
+            AS rank
+    FROM
+        products AS p 
+    JOIN 
+        categories AS c 
+    USING 
+        (category_id)
+)
+SELECT
+    c.category_name,
+    p.product_name
+FROM
+    products AS p 
+JOIN 
+    categories AS c 
+USING 
+    (category_id)
+JOIN 
+    rank_categories
+USING  
+    (product_name)
+WHERE
+    rank = 3
+ORDER BY 
+    category_name ASC
+;
